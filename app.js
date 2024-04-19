7a11a0b2085e06fb3fdb3892ac8d7128d6ce20ec
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-04-19 20:31:46 +0500

    Changes in app.js

--- app.js
+++ app.js
@@ -87,13 +87,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//Alert showing on any create, edit, update and delete request
+ // Alert showing on any create, edit, update and delete request
 // app.use((req, res, next) => {
 //     res.locals.success = req.flash("success");
 //     res.locals.error = req.flash("error");
 //     res.locals.currentUser = req.user;
 //     next();
 // });
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
